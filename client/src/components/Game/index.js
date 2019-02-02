1cb601ad17c71a3512159e2504893aace5628307
MiChel Causey <michel.causey@gmail.com>
2019-02-02 18:29:16 -0500

    mac miller - swimming (how do i make this less repetitive)

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -86,6 +86,51 @@ class Game extends Component {
       console.log("you know you can't win this one");
       this.setState({location: this.state.location = 14})
     }
+    // location: 15 = Join Coven
+    else if (event.target.innerText === "Join Coven") {
+      console.log("your a witch, hermoine");
+      this.setState({location: this.state.location = 15})
+    }
+    // location: 16 = Reject Invitation
+    else if (event.target.innerText === "Reject Invitation") {
+      console.log("what did you call me?");
+      this.setState({location: this.state.location = 16})
+    }
+    // location: 17 = Climb Down
+    else if (event.target.innerText === "Climb Down") {
+      console.log("seems like a bad plan");
+      this.setState({location: this.state.location = 17})
+    }
+    // location: 18 = Use the Parachute
+    else if (event.target.innerText === "Use the Parachute") {
+      console.log("try it out");
+      this.setState({location: this.state.location = 18})
+    }
+    // location: 19 = Look
+    else if (event.target.innerText === "Look") {
+      console.log("(o)-(o)");
+      this.setState({location: this.state.location = 19})
+    }
+    // location: 20 = Keep Your Eyes Closed
+    else if (event.target.innerText === "Keep Your Eyes Closed") {
+      console.log("(x)-(x)");
+      this.setState({location: this.state.location = 20})
+    }
+    // location: 21 = Sneak There
+    else if (event.target.innerText === "Sneak There") {
+      console.log("shh");
+      this.setState({location: this.state.location = 21})
+    }
+    // location: 22 = Run!
+    else if (event.target.innerText === "Run!") {
+      console.log("goooooo");
+      this.setState({location: this.state.location = 22})
+    }
+    // location: 23 = Participate
+    else if (event.target.innerText === "Participate") {
+      console.log("be flexible");
+      this.setState({location: this.state.location = 23})
+    }
   };
 
   render() {
