6e92b3d9159c14c416972808c01ad0d4873eb3d2
MiChel Causey <michel.causey@gmail.com>
2019-02-09 09:55:18 -0500

    forms updated

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,37 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
 import Footer from "../Footer"
 import "./style.css";
 
-function Login() {
-    return (
+class Login extends Component {
+    // Setting the component's initial state
+    state = {
+      userName: "",
+      password: ""
+    };
+  
+    handleInputChange = event => {
+      // Getting the value and name of the input which triggered the change
+      let value = event.target.value;
+      const name = event.target.name;
+  
+      if (name === "password") {
+        value = value.substring(0, 15);
+      }
+      // Updating the input's state
+      this.setState({
+        [name]: value
+      });
+    };
+  
+    handleFormSubmit = event => {
+      // Preventing the default behavior of the form submit (which is to refresh the page)
+      event.preventDefault();
+      if (!this.state.userName || !this.state.password) {
+        alert("You must enter your username & password");
+      } else {
+        alert(`Welcome back ${this.state.userName}, you have successfully logged in`);
+        
+      }
+  
+      this.setState({
+        userName: "",
+        password: ""
+      });
+    };
+  
+    render() {
+      // Notice how each input has a `value`, `name`, and `onChange` prop
+      return (
         <div className="container-login">
-            <div className="login-header">
-                <h1>Welcome Back</h1>
-            </div>
-        <div className="whole-form">
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email: example_name@email.com" />
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Type Your Password" />
-        </div>
-        
-        <a id="no-hover" href="/game"><button type="submit" id="submit-button" class="btn btn-primary">Submit</button></a>
-        <br />
-        <h4 id="register-login">
-          Don't have an account?{" "}
-          <a href="/Register" id="signup-link">
-            Sign up now
-          </a>
-        </h4>
-        
-        <Footer />
-        </div>
-        </div>
-    );
+          <p id="welcome-text">
+            Welcome Back, log-in to continue
+          </p>
+          <form className="form">
+            <input id="username"
+              value={this.state.userName}
+              name="userName"
+              onChange={this.handleInputChange}
+              type="text"
+              placeholder="Email Address"
+            /><br></br>
+            <input id="password"
+              value={this.state.password}
+              name="password"
+              onChange={this.handleInputChange}
+              type="password"
+              placeholder="Password"
+            />
+            <br></br>
+            <button id="submit-button" onClick={this.handleFormSubmit}>Submit</button>
+            <br></br>
+            <p id="register-link">Don't have a username/password? <a href="/register">sign-up here</a></p>
+          </form>
+          <Footer />
+          </div>
+      );
+    }
   }
+  
 
 export default Login;
+
+
+
 
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,42 +1,105 @@
-import React from "react";
-import Footer from "../Footer"
+import React, { Component } from "react";
+import Footer from "../Footer";
 import "./style.css";
 
+class Register extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    password: ""
+  };
 
-function Register() {
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.firstName || !this.state.lastName) {
+      alert("Please enter your first & last name");
+    } else if (!this.state.userName) {
+      alert("Please select a username");
+    } else if (!this.state.password) {
+      alert("Please select a password");
+    } else {
+      alert(`Welcome ${this.state.firstName} ${this.state.lastName}, you have successfully created an account`);
+    }
+
+    this.setState({
+      firstName: "",
+      lastName: "",
+      userName: "",
+      password: ""
+    });
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
     return (
-        <div className="container-register">
-            <div className="register-header">
-                <h1>Sign-Up to Play:</h1>
-            </div>
-        <div className="whole-form">
-        <div class="form-group">
-            <label for="name">First & Last Name</label>
-            <input type="name" class="form-control" id="exampleName" aria-describedby="nameHelp" placeholder="Enter name (ex: Jane Doe)" />
-        </div>
-        <div class="form-group">
-            <label for="emailAddress">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email (example_name@email.com)" />
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Create Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Type Your Password" />
-        </div>
-        
-        <a id="no-hover" href="/game"><button type="submit" id="register-button" class="btn btn-primary">Submit</button></a>
-        <br />
-        <h4 id="register-login">
-          Already have an account?{" "}
-          <a href="/login" id="signup-link">
-            Log-In
-          </a>
-        </h4>
-        
+      <div className="container-login">
+        <p id="welcome-text">Welcome, sign-up below</p>
+        <form className="form">
+          <input
+            id="firstName"
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <br />
+          <input
+            id="lastName"
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <br />
+          <input
+            id="username"
+            value={this.state.userName}
+            name="userName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username"
+          />
+          <br />
+          <input
+            id="password"
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+          <button id="submit-button" onClick={this.handleFormSubmit}>
+            Submit
+          </button>
+          <br />
+          <p id="register-link">
+            Already have a username/password? <a href="/login">log-in here</a>
+          </p>
+        </form>
         <Footer />
-        </div>
-        </div>
+      </div>
     );
   }
+}
 
 export default Register;
-
