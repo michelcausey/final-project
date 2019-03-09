766af7b37522d477ddc517b1f40fd5f3c399a692
MiChel Causey <michel.causey@gmail.com>
2019-03-08 20:46:38 -0500

    styling on all headers

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -28,12 +28,12 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar color="muted" light expand="md">
-          <NavbarBrand href="/" id="h5"><img src="images/compass.png" alt="compass"></img>Code Your Own Adventure<img src="images/compass.png"></img></NavbarBrand>
+          <NavbarBrand href="/" id="h5"><img src="images/compass.png" alt="compass"></img>CYOA</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/login"><img src="images/login.png"alt="login" alt="log-in"></img></NavLink>
+                <NavLink href="/login"><img id="login-icon" src="images/login.png"alt="login" alt="log-in"></img></NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="/register"><img src="images/new-user.png"alt="new-user"></img></NavLink>
